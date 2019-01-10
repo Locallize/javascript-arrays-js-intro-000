294dcc7e25e31129d60617094c8fa33a88e06a15
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-10 02:48:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array;
 }
 function addElementToEndOfArray(array,element){
-  
   return [...array,element];
 }
 function accessElementInArray(array, index){
